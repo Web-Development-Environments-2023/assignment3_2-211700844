17f968293e0f5a4e83d3c0a7462c8cb5ba0a31cd
MoriaCohen8 <103572180+MoriaCohen8@users.noreply.github.com>
2023-07-23 12:46:18 +0300

    Update user_utils.js
    documentation

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -1,5 +1,10 @@
 const DButils = require("./DButils");
 
+/**
+ * Marks a recipe as favorite for a specific user.
+ * @param {number} user_id - The ID of the user.
+ * @param {string} recipe_id - The ID of the recipe to mark as favorite.
+ */
 async function markAsFavorite(user_id, recipe_id){
     const recipe_fav = (
         await DButils.execQuery(
@@ -11,11 +16,30 @@ async function markAsFavorite(user_id, recipe_id){
     }
 }
 
+/**
+ * Retrieves the list of favorite recipes for a specific user.
+ * @param {number} user_id - The ID of the user.
+ * @returns {Array} - An array of recipe IDs that are marked as favorite by the user.
+ */
 async function getFavoriteRecipes(user_id){
     const recipes_id = await DButils.execQuery(`select recipe_id from favoriterecipes where user_id='${user_id}'`);
     return recipes_id;
 }
-
+/**
+ * Adds a personal recipe to the user's collection.
+ * @param {number} user_id - The ID of the user.
+ * @param {string} title - The title of the personal recipe.
+ * @param {number} readyInMinutes - The preparation time in minutes for the personal recipe.
+ * @param {string} image - The image URL of the personal recipe.
+ * @param {number} aggregateLikes - The number of likes for the personal recipe.
+ * @param {boolean} vegan - Indicates if the personal recipe is vegan.
+ * @param {boolean} vegetarian - Indicates if the personal recipe is vegetarian.
+ * @param {boolean} glutenFree - Indicates if the personal recipe is gluten-free.
+ * @param {number} servings - The number of servings for the personal recipe.
+ * @param {string} analyzedInstructions - The cooking instructions for the personal recipe.
+ * @param {string} extendedIngredients - The list of ingredients for the personal recipe.
+ * @returns {string} - A message indicating the success of adding the personal recipe.
+ */
 async function addPersonalRecipe(user_id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree, servings, analyzedInstructions, extendedIngredients){ 
     // await DButils.execQuery(
     //     `INSERT INTO personalrecipes (user_id, title, readyInMinutes, image, aggregateLikes,vegan, vegetarian, glutenFree, servings, analyzedInstructions, extendedIngredients) VALUES ('${user_id}','${title}', '${readyInMinutes}', '${image}','${aggregateLikes}', ${vegan}, ${vegetarian}, ${glutenFree}, '${servings}', '${analyzedInstructions}', '${extendedIngredients}')`
@@ -38,6 +62,11 @@ async function addPersonalRecipe(user_id, title, readyInMinutes, image, aggregat
     return "The Recipe successfully saved in your personal recipes list";
 }
 
+/**
+ * Retrieves the list of personal recipes for a specific user.
+ * @param {number} user_id - The ID of the user.
+ * @returns {Array} - An array of personal recipes with preview details.
+ */
 async function getPersonalRecipes(user_id){
     const recipes = (
         await DButils.execQuery(
@@ -62,7 +91,11 @@ async function getPersonalRecipes(user_id){
     return reciped_preview;
     
 }
-
+/**
+ * Adds a recipe to the user's history of viewed recipes.
+ * @param {number} user_id - The ID of the user.
+ * @param {string} recipe_id - The ID of the recipe viewed by the user.
+ */
 async function addToThreeLastOfUser(user_id, recipe_id){
     let recipe_check = await DButils.execQuery(`select * from historyofuser where user_id='${user_id}'`);
     if (recipe_check.length==0){
@@ -98,6 +131,11 @@ async function addToThreeLastOfUser(user_id, recipe_id){
         
     }
 }
+/**
+ * Retrieves the last three viewed recipes of a specific user.
+ * @param {number} user_id - The ID of the user.
+ * @returns {Array} - An array of the last three viewed recipe IDs by the user.
+ */
 async function getThreeLastOfUser(user_id){
     let Viewed=[]
     let recipe_check = await DButils.execQuery(`select last1, last2, last3 from historyofuser where user_id='${user_id}'`);
@@ -116,7 +154,11 @@ async function getThreeLastOfUser(user_id){
     }
     return Viewed;
 }
-
+/**
+ * Updates the seen history for a specific user and recipe.
+ * @param {number} user_id - The ID of the user.
+ * @param {string} recipe_id - The ID of the recipe viewed by the user.
+ */
 async function updateseen(user_id, recipe_id){
     const recipe_info = (
         await DButils.execQuery(
@@ -127,7 +169,11 @@ async function updateseen(user_id, recipe_id){
         await DButils.execQuery(`insert into seen values ('${user_id}','${recipe_id}')`);
     }
 }
-
+/**
+ * Retrieves the list of family recipes for a specific user.
+ * @param {number} user_id - The ID of the user.
+ * @returns {Array} - An array of family recipes with details.
+ */
 async function getFamilyRecipes(user_id){
     const recipes=await DButils.execQuery(`select * from familyrecipes where user_id='${user_id}'`);
     if(recipes.length == 0)
@@ -171,7 +217,11 @@ async function getFamilyRecipes(user_id){
 
     return response;
 }
-
+/**
+ * Adds a recipe to the user's meal plan.
+ * @param {number} user_id - The ID of the user.
+ * @param {string} recipe_id - The ID of the recipe to add to the meal plan.
+ */
 async function addRecipeToMeal(user_id, recipe_id){
     const recipe = (
         await DButils.execQuery(
@@ -182,15 +232,23 @@ async function addRecipeToMeal(user_id, recipe_id){
         await DButils.execQuery(`insert into planmeal values ('${user_id}','${recipe_id}')`);
     }
 }
-
+/**
+ * Deletes a recipe from the user's meal plan.
+ * @param {number} user_id - The ID of the user.
+ * @param {string} recipe_id - The ID of the recipe to delete from the meal plan.
+ */
 async function deleteRecipeToMeal(user_id, recipe_id){
     await DButils.execQuery(`DELETE FROM planmeal where user_id='${user_id}' AND recipe_id='${recipe_id}'`)
 }
 
+/**
+ * Clears the user's meal plan.
+ * @param {number} user_id - The ID of the user.
+ */
 async function cleanRecipeToMeal(user_id){
     await DButils.execQuery(`DELETE FROM planmeal where user_id='${user_id}'`)
 }
-
+// Exported functions
 exports.markAsFavorite = markAsFavorite;
 exports.getFavoriteRecipes = getFavoriteRecipes;
 exports.getThreeLastOfUser = getThreeLastOfUser;
